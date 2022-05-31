03bb71e5cb2c9af80d05ab9dd0e06d88a49cc1e8
GyuryHan <pos7271@daum.net>
2022-05-31 18:48:25 +0900

    api 패칭 로직 추가

--- src/store/ui-slice.js
+++ src/store/ui-slice.js
@@ -1,6 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialUIState = { visible: false };
+const initialUIState = { visible: false, notification: null };
 
 const uiSlice = createSlice({
   name: "cart",
@@ -8,6 +8,13 @@ const uiSlice = createSlice({
   reducers: {
     isVisible(state) {
       state.visible = !state.visible;
+    },
+    showNotification(state, action) {
+      state.notification = {
+        status: action.payload.status,
+        title: action.payload.title,
+        message: action.payload.message,
+      };
     },
   },
 });
